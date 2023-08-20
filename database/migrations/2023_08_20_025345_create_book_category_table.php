e18e9108fbbba8efb244969ed763ca1b404215f2
aldod400 <aldodelghonemy400@gmail.com>
2023-08-20 06:45:47 +0300

    add migrations only

--- database/migrations/2023_08_20_025345_create_book_category_table.php
+++ database/migrations/2023_08_20_025345_create_book_category_table.php
@@ -11,12 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->unsignedInteger('role_id')->references('id')->on('roles');
-            $table->rememberToken();
+        Schema::create('book_category', function (Blueprint $table) {
+            $table->unsignedInteger('book_id')->references('id')->on('books');
+            $table->unsignedInteger('category_id')->references('id')->on('categories');
         });
     }
 
@@ -25,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('book_category');
     }
 };
