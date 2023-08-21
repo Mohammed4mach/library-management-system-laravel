a23a52f947fcf378848cf6b3d20e68c983aa62ab
Muhammed4mach <moha4mach@gmail.com>
2023-08-21 07:00:11 +0300

    Update Migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,15 +6,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     public function role(){
 
-    return $this->belongsTo(role::class);
+    return $this->belongsTo(Role::class);
     }
 
     public function BorrowedBook(){
--- database/migrations/2023_08_20_024649_create_borrowed_books_table.php
+++ database/migrations/2023_08_20_024649_create_borrowed_books_table.php
@@ -13,11 +13,15 @@ return new class extends Migration
     {
         Schema::create('borrowed_books', function (Blueprint $table) {
             $table->id();
-            $table->unsignedInteger('user_id')->references('id')->on('users');
-            $table->unsignedInteger('book_id')->references('id')->on('books');
-            $table->boolean('returned');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('book_id');
+            $table->boolean('returned')->default(false);
             $table->date('return_date');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('book_id')->references('id')->on('books');
+
         });
     }
 
