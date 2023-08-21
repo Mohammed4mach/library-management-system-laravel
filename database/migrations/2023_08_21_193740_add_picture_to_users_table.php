1a6ea173c6575a7edb023777774e6a3e7e1c8518
Muhammed4mach <moha4mach@gmail.com>
2023-08-22 01:47:22 +0300

    Create Controllers & Requests

--- database/migrations/2023_08_21_193740_add_picture_to_users_table.php
+++ database/migrations/2023_08_21_193740_add_picture_to_users_table.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -12,15 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('roles', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
+        Schema::table('users', function (Blueprint $table) {
+            // Add picture path column
+            $table->string('picture')->nullable()->default(null);
         });
-
-        DB::table('roles')->insert([
-            [ 'title' => 'admin' ],
-            [ 'title' => 'student' ],
-        ]);
     }
 
     /**
@@ -28,6 +22,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('roles');
+        Schema::table('users', function (Blueprint $table) {
+            // Drop picture column
+            $table->dropColumn('picture');
+        });
     }
 };
